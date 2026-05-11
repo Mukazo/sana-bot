11a51f2f382ab804b69c653ab75e4e96f4d48684
Kentrell <graylight2607@gmail.com>
2026-05-11 19:33:54 -0400

    Ekw

--- commands/global/log-in.js
+++ commands/global/log-in.js
@@ -3,7 +3,7 @@ const {
   EmbedBuilder,
 } = require('discord.js');
 
-const User = require('../models/User');
+const User = require('../../models/User');
 
 module.exports = {
   data: new SlashCommandBuilder()
