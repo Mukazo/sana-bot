3fe1be0d5fe39589277e0d8a08064bba0f343f86
Kentrell <graylight2607@gmail.com>
2026-05-11 20:30:31 -0400

    kefw

--- commands/global/balance.js
+++ commands/global/balance.js
@@ -19,7 +19,11 @@ module.exports = {
 
     const embed = new EmbedBuilder()
   .setColor('#D8E34B')
-  .setDescription(`## ✧ ࣪.•  ꒰ <:keroppi:1502031714778742784> ꒱ __**${targetUser.user} Keroppi Bank**__ !! 𓈒⁺*♪* ♫\n\n ꒷₊˚ <:kittokens:1501647903486116081> __Kittokens__ : **${userData.kittokens.toLocaleString()}**\n  .     𓂃 ଓ  ۪   ݁   ⌁\n ꒷₊˚ <:pawprints:1501648560700002506> __Pawprints__ : **${userData.pawprints.toLocaleString()}**`)
+  .setAuthor({
+    name: targetUser.username,
+    iconURL: targetUser.displayAvatarURL({ dynamic: true }),
+  })
+  .setDescription(`✧ ࣪.•  ꒰ <:keroppi:1502031714778742784> ꒱ __**Keroppi Bank**__ !! 𓈒⁺*♪* ♫\n\n ꒷₊˚ <:kittokens:1501647903486116081> __Kittokens__ : **${userData.kittokens.toLocaleString()}**\n  .     𓂃 ଓ  ۪   ݁   ⌁\n ꒷₊˚ <:pawprints:1501648560700002506> __Pawprints__ : **${userData.pawprints.toLocaleString()}**`)
 
     return safeReply(interaction, { embeds: [embed] });
   }
