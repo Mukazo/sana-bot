b47d19eeddeed8952fe4cffc8de112cdbac66424
Kentrell <graylight2607@gmail.com>
2026-05-11 20:22:42 -0400

    fekw

--- commands/global/balance.js
+++ commands/global/balance.js
@@ -5,7 +5,7 @@ const { safeReply } = require('../../utils/safeReply');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('balance')
-    .setDescription('view yours or anothers balance')
+    .setDescription(`view yours or another player's balance`)
     .addUserOption(opt =>
       opt.setName('user')
         .setDescription('provide another user to view')
@@ -19,7 +19,7 @@ module.exports = {
 
     const embed = new EmbedBuilder()
   .setColor('#D8E34B')
-  .setDescription(`✧ ࣪.•  ꒰ <:keroppi:1502031714778742784> ꒱ __**${targetUser.username} Keroppi Bank**__ !! 𓈒⁺*♪* ♫\n ꒷₊˚ <:kittokens:1501647903486116081> __Kittokens__ : **${userData.kittokens.toLocaleString()}**\n  .     𓂃 ଓ  ۪   ݁   ⌁\n ꒷₊˚ <:pawprints:1501648560700002506> __Pawprints__ : **${userData.pawprints.toLocaleString()}**`)
+  .setDescription(`## ✧ ࣪.•  ꒰ <:keroppi:1502031714778742784> ꒱ __**${targetUser.user} Keroppi Bank**__ !! 𓈒⁺*♪* ♫\n\n ꒷₊˚ <:kittokens:1501647903486116081> __Kittokens__ : **${userData.kittokens.toLocaleString()}**\n  .     𓂃 ଓ  ۪   ݁   ⌁\n ꒷₊˚ <:pawprints:1501648560700002506> __Pawprints__ : **${userData.pawprints.toLocaleString()}**`)
 
     return safeReply(interaction, { embeds: [embed] });
   }
