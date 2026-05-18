3a2b1779cf521ba7ed3629a01dcf70b41527dd09
Kentrell <graylight2607@gmail.com>
2026-05-18 10:35:42 -0400

    meow

--- commands/guild-only/card.js
+++ commands/guild-only/card.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const createCard = require('../subcommands/card/create.js');
 const editCard = require('../subcommands/card/edit.js');
 
@@ -6,7 +6,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('card')
     .setDescription('Manage cards')
-    .setDefaultMemberPermissions('0')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageWebhooks)
     .addSubcommand(sub =>
       sub.setName('create')
         .setDescription('create a card')
